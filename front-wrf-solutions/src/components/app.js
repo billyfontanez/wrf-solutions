232a8ede6c8e66ed6fdab66a538971c725b5f9fa
William Fontanez <william.r.fontanez@gmail.com>
2021-11-18 17:09:37 -0600

    Added header component with logo/gif

--- front-wrf-solutions/src/components/app.js
+++ front-wrf-solutions/src/components/app.js
@@ -3,13 +3,13 @@ import { useRoutes } from 'hookrouter';
 import NavBar from './Layout/navBar';
 import routes from './Layout/routes';
 import history from './PrivatePages/history';
-
+import Header from './Layout/header';
 
 export default function App() {
   const routeResult = useRoutes(routes);
     return (
       <div className='app'>
-        <h1>WRF Solutions</h1>
+        <Header />
         <NavBar />
         {routeResult || <h1>Page Not Found</h1>}
       </div>
