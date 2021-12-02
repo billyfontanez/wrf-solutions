89984eb4335c3de736d371de8ed85587b29ad606
William Fontanez <william.r.fontanez@gmail.com>
2021-12-02 12:39:53 -0600

    Edited Header

--- front-wrf-solutions/src/components/layout/header.js
+++ front-wrf-solutions/src/components/layout/header.js
@@ -1,9 +1,13 @@
 import React, { Component } from "react";
+import Logo from '../../../static/assets/Logos/WRF3-240x240.gif';
+
+console.log(Logo);
 
 export default class Header extends Component{
     render(){
         return (
             <div className='logo'>
+                <img src='../../../static/assets/Logos/WRF3-240x240.gif' alt='logo'/>
             </div>
         );
     }
